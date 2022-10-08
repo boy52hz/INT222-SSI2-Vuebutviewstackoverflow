dc05851723c37233289daeb36cea3b220e78180d
boy52hz <ggwp.zero@gmail.com>
2022-10-08 23:25:23 +0700

    fixed create user modal

--- client/src/components/VSchedules/UserFormInput.vue
+++ client/src/components/VSchedules/UserFormInput.vue
@@ -57,72 +57,72 @@ onBeforeMount(async () => {
   props.userModel.role = roleOptions.value[0].value
 })
 
-defineEmits(['add-user','cancel-form'])
+defineEmits(['add-user', 'cancel-form'])
 </script>
 
 <template>
-    <app-modal :show="modalState.state">
-        <form 
-        @submit.prevent="$emit('add-user')"
-        @reset.prevent="$emit('cancel-form')"
-        >
-        <h3>Create New Account</h3>
-            <div>
-                <AppInput
-                  type="text"
-                  v-model.trim="userModel.name"
-                  label-text="Name"
-                  :maxlength="100"
-                  :error-message="errorMessage.name"
-                  :required="true"
-                />
-                <AppInput
-                  type="email"
-                  pattern="^[^(\.)][a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}"
-                  v-model.trim="userModel.email"
-                  label-text="Email"
-                  :maxlength="50"
-                  :error-message="errorMessage.email"
-                  :required="true"
-                />
-                <AppInput
-                  type="password"
-                  v-model="userModel.password"
-                  label-text="Password"
-                  @focusout="validatePasswordMatch"
-                  :minlength="8"
-                  :error-message="errorMessage.password"
-                  :required="true"
-                />
-                <AppInput
-                  type="password"
-                  v-model="userModel.confirmPassword"
-                  label-text="Confirm Password"
-                  @focusout="validatePasswordMatch"
-                  :minlength="8"
-                  :error-message="errorMessage.confirmPassword"
-                  :required="true"
-                />
-                <AppInput
-                  v-model.trim="userModel.role"
-                  label-text="Role"
-                  :options="roleOptions"
-                  :error-message="errorMessage.role"
-                  :required="true"
-                />
-                <div class="app-button-group">
-                    <app-button class="btn-submit" button-type="success" >Create</app-button>
-                    <app-button class="btn-cancel" button-type="danger" type="reset" @click="modalState.close()">Cancel</app-button>
-                </div>
-            </div>
-        </form>
-    </app-modal>
+  <app-modal :show="modalState.state">
+    <form @submit.prevent="$emit('add-user')" @reset.prevent="$emit('cancel-form')">
+      <h3>Create New Account</h3>
+      <div>
+        <AppInput
+          type="text"
+          v-model.trim="userModel.name"
+          label-text="Name"
+          :maxlength="100"
+          :error-message="errorMessage.name"
+          :required="true"
+        />
+        <AppInput
+          type="email"
+          pattern="^[^(\.)][a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}"
+          v-model.trim="userModel.email"
+          label-text="Email"
+          :maxlength="50"
+          :error-message="errorMessage.email"
+          :required="true"
+        />
+        <AppInput
+          type="password"
+          v-model="userModel.password"
+          label-text="Password"
+          @focusout="validatePasswordMatch"
+          :minlength="8"
+          :error-message="errorMessage.password"
+          :required="true"
+        />
+        <AppInput
+          type="password"
+          v-model="userModel.confirmPassword"
+          label-text="Confirm Password"
+          @focusout="validatePasswordMatch"
+          :minlength="8"
+          :error-message="errorMessage.confirmPassword"
+          :required="true"
+        />
+        <AppInput
+          v-model.trim="userModel.role"
+          label-text="Role"
+          :options="roleOptions"
+          :error-message="errorMessage.role"
+          :required="true"
+        />
+        <div class="app-button-group">
+          <app-button class="btn-submit" button-type="success">Create</app-button>
+          <app-button class="btn-cancel" button-type="danger" type="reset" @click="modalState.close()"
+            >Cancel</app-button
+          >
+        </div>
+      </div>
+    </form>
+  </app-modal>
 </template>
 <style>
 .app-button-group {
-    display: flex;
+  display: flex;
 }
-.btn-cancel,.btn-submit{
-    width: 50%;
+.btn-cancel,
+.btn-submit {
+  width: 50%;
 }
 </style>
