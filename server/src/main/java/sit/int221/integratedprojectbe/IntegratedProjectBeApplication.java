166b1bab78b65facc929e4f2a48a5caddeeea325
boy52hz <ggwp.zero@gmail.com>
2022-11-01 22:21:37 +0700

    try to fix findById cannot used

--- server/src/main/java/sit/int221/integratedprojectbe/IntegratedProjectBeApplication.java
+++ server/src/main/java/sit/int221/integratedprojectbe/IntegratedProjectBeApplication.java
@@ -1,24 +1,16 @@
 package sit.int221.integratedprojectbe;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import sit.int221.integratedprojectbe.repositories.EventRepository;
 
-
-import javax.mail.MessagingException;
 import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableAsync
 public class IntegratedProjectBeApplication {
-    @Autowired
-    EventRepository eventRepository;
     public static void main(String[] args) {
         SpringApplication.run(IntegratedProjectBeApplication.class, args);
     }
