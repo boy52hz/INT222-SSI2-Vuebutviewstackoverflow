d7363ef8f4ee950f0e078699afddf7fa37f99091
boy52hz <ggwp.zero@gmail.com>
2022-11-30 04:20:17 +0700

    edit env config sample and use case

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ command, mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-    base: process.env.BASE_URL,
+    base: process.env.VITE_BASE_PATH,
     server: {
       port: 3000,
       proxy: {
